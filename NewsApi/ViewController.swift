46e0360e06a962262be9ed99cb7aefa4ddcc761f
Salih Cakmak <slhckmk01@gmail.com>
2023-03-23 12:01:06 +0300

    Initial Commit

---
+++ NewsApi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApi
+//
+//  Created by Salih on 23.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
